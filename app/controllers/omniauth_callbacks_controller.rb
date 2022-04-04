2fc25998359ab9f6bd93624c553996c1f4fc7ad0
nicopleyto <nicopleyto@gmail.com>
2022-04-04 15:12:30 +0800

    configured route names for clarity

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,11 @@
 class OmniauthCallbacksController < ApplicationController
-  def twitter
+  def connect_twitter
     render plain: "Success!"
+  end
+
+  private
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'home/main'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/auth/twitter/callback", to: "omniauth_callbacks#twitter"
+  get "/auth/twitter/callback", to: "omniauth_callbacks#connect_twitter"
   root 'home#main'
 end
