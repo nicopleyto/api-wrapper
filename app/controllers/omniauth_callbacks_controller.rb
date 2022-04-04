7e3487d0a00a6597e9d9a21bf73a8e685d3dc2bc
nicopleyto <nicopleyto@gmail.com>
2022-04-04 17:13:58 +0800

    added index and delete for twitter accounts

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,13 +1,13 @@
 class OmniauthCallbacksController < ApplicationController
   def connect_twitter
-    @twitter_account = current_user.twitter_accounts.find_or_create_by(username: auth.info.nickname) do |account|
-      account.name: auth.info.name,
-      account.image: auth.info.image,
-      account.token: auth.credentials.token,
-      account.secret: auth.credentials.secret
+    twitter_account = current_user.twitter_accounts.find_or_create_by(username: auth_hash.info.nickname) do |account|
+      account.name = auth_hash.info.name,
+      account.image = auth_hash.info.image,
+      account.token = auth_hash.credentials.token,
+      account.secret = auth_hash.credentials.secret
     end
 
-    redirect_to root_path, notice: "Twitter account has been successfully connected."
+    redirect_to twitter_accounts_path, notice: "Twitter account has been successfully connected."
   end
 
   private
