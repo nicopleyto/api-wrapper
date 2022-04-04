2ba97e144ecf0954242167694f2522441d4d9f94
nicopleyto <nicopleyto@gmail.com>
2022-04-04 15:26:41 +0800

    added navbar

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,10 +1,12 @@
 class OmniauthCallbacksController < ApplicationController
   def connect_twitter
     render plain: "Success!"
+    byebug
   end
 
   private
 
+  #helper method from omniauth which contains a hash of information about the user when sent to the callback url
   def auth_hash
     request.env['omniauth.auth']
   end
