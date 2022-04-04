1c07c62e9fa806df63901bc51fcc612bd3934eea
nicopleyto <nicopleyto@gmail.com>
2022-04-04 16:14:50 +0800

    added method which creates twitter accounts and prevents duplicates from being saved

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,13 @@
 class OmniauthCallbacksController < ApplicationController
   def connect_twitter
-    render plain: "Success!"
-    
+    @twitter_account = current_user.twitter_accounts.find_or_create_by(username: auth.info.nickname) do |twitter_account|
+      twitter_account.name: auth.info.name,
+      twitter_account.image: auth.info.image,
+      twitter_account.token: auth.credentials.token,
+      twitter_account.secret: auth.credentials.secret
+    end
+
+    redirect_to root_path, notice: "Twitter account has been successfully connected."
   end
 
   private
