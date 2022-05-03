b5e89c923bd1bd54843b36830ce8dd8832005030
nicopleyto <nicopleyto@gmail.com>
2022-05-04 03:32:43 +0800

    added tweet validations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,12 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   belongs_to :twitter_account
+
+  validates :body, length: {minimum: 1, maximum: 280}
+  validates :publish_at, presence: true
+
+  # sets the default value of publish_at datetime_select to 1 day later upon initialize
+  after_initialize do
+    self.publish_at ||= 24.hours.from.now
+  end
 end
