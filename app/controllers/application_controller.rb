c9d579a2cdf6719689ff449f5044921820c780bf
nicopleyto <nicopleyto@gmail.com>
2022-05-05 06:27:21 +0800

    solved bug in set time zone method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,11 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        #devise_parameter_sanitizer.permit(:sign_up, keys: [:time_zone])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:time_zone])
         devise_parameter_sanitizer.permit(:account_update, keys: [:time_zone])
     end
 
     def set_time_zone(&block)
-        if current_user.time_zone?
-            Time.use_zone(current_user.time_zone, &block)
-        end
+        Time.use_zone(current_user.time_zone, &block)
     end
 end
