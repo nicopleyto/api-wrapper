e6b973649ce0b852af142eaa839e79ea5b551611
nicopleyto <nicopleyto@gmail.com>
2022-05-04 20:42:00 +0800

    added client method for authentication, added publish to twitter method to publish tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -13,4 +13,10 @@ class Tweet < ApplicationRecord
   def published?
     self.tweet_id? #adding ? to a column checks whether there is value by returning a boolean
   end
+
+  def publish_to_twitter!
+    # twitter gem acts as an api wrapper and gives access to client.update("tweet body here") method 
+    tweet = self.twitter_account.client.update(body) #JSON response is captured via tweet local variable
+    self.update(tweet_id: tweet.id) #JSON response contains tweet id which we can use to populate tweet_id column of tweet
+  end
 end
--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -2,4 +2,14 @@ class TwitterAccount < ApplicationRecord
   belongs_to :user
   has_many :tweets
   validates :username, uniqueness: true #prevents record from being created twice
+
+  def client
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = Rails.application.credentials.api_key
+      config.consumer_secret     = Rails.application.credentials.api_key_secret
+      config.access_token        = self.token
+      config.access_token_secret = self.secret
+    end
+  end
+
 end
