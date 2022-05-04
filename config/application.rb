7ae6ca07be6d9fb42ce4aef1803b92302aa8b469
nicopleyto <nicopleyto@gmail.com>
2022-05-05 05:05:25 +0800

    added time zone toggle option for edit profile

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Apiwrapper
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Singapore"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
