35a76c3a4620485126efecc5cf0bcee31cb31a21
nicopleyto <nicopleyto@gmail.com>
2022-05-06 01:59:04 +0800

    minor change to set initial time zone method

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,12 @@ class User < ApplicationRecord
 
   after_create :set_initial_time_zone
 
-  #converts client side timezone to rails timezone upon creation of user
+  #converts client side timezone to rails timezone upon creation of user. limited to scope of rake:time:zones:all
   def set_initial_time_zone 
-    js_timezone = self.time_zone
-    self.update(time_zone: ActiveSupport::TimeZone::MAPPING.key(js_timezone))
+    if self.time_zone?
+      js_timezone = self.time_zone
+      self.update(time_zone: ActiveSupport::TimeZone::MAPPING.key(js_timezone))
+    end
   end
 
 end
