cfd8389fa3d0be0d4836a64172b762c74d5e8173
nicopleyto <nicopleyto@gmail.com>
2022-05-06 16:29:02 +0800

    fixed minor bug on tweet job

--- app/jobs/tweet_job.rb
+++ app/jobs/tweet_job.rb
@@ -3,7 +3,7 @@ class TweetJob < ApplicationJob
 
   def perform(tweet)
     return if tweet.published? #returns if tweet_id column is already populated
-    return if tweet.published_at > Time.current #returns if updated tweet date/time is moved to later date/time to prevent jobs from posting twice
+    return if tweet.publish_at > Time.current #returns if updated tweet date/time is moved to later date/time to prevent jobs from posting twice
     tweet.publish_to_twitter!
   end
 end
